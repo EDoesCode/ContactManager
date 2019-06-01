25eebeccf5ea1fb892fe7f5d91a06b2b2a0a16f5
LionHearted56 <nelsoncaster34@keemail.me>
2019-06-01 13:42:03 -0400

    Added JS files for login and register and modified html files to work with the js

---
+++ html/js/register_page.js
@@ -0,0 +1,65 @@
+var baseURL = "https://cop4331.a2hosted.com/";
+var url = "https://cop4331.a2hosted.com/api/Register.php";
+
+var firstName = "";
+var lastName = "";
+var accountName = "";
+var password = "";
+
+function register()
+{
+	firstName = document.getElementById("firstName").value;
+	lastName = document.getElementById("lastName").value;
+	accountName = document.getElementById("accountName").value;
+	password = document.getElementById("password").value;
+	var rpassword = document.getElementById("repeatPassword").value;
+
+	if(password.length < 4)
+	{
+		alert("Password has to be more than 4 characters!!");
+		document.getElementById("password").value = "";
+		document.getElementById("repeatPassword").value = "";
+		return;
+	}
+
+	if(password != rpassword)
+	{
+		alert("Password and Repeat Password are not the same!!");
+		document.getElementById("password").value = "";
+		document.getElementById("repeatPassword").value = "";
+		return;
+	}
+
+	var newPassword = accountName + password;
+
+	var SHA512 = new Hashes.SHA512;
+
+	newPassword = SHA512.hex(newPassword);
+
+	var jsonPayload = '{"FirstName" : ' + firstName + ', "LastName" : ' + lastName + ', "AccountName" : ' + accountName + ', "Password" : ' + newPassword + '}';
+
+	var xhr = XMLHttpRequest();
+	xhr.open("POST", url, true,);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try
+	{
+		var jsonResponse = JSON.parse(xhr.responseText);
+
+		xhr.onreadystatechange = function()
+		{
+			if(this.readyState == 4 && this.status == 200)
+			{
+				if(jsonResponse.error.length == 0)
+					window.location = baseURL + "contact_page.html";
+				else
+					alert(jsonResponse.error);
+			}
+		}
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		alert("Invalid Connection!!");
+	}
+}
